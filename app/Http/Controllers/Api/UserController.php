71c304587c0e9b9a38b1f720b511930699f46709
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2024-05-15 08:45:52 +0700

    init

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Hello World!'
+        ]);
+    }
+
+    public function profile()
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User profile',
+            'data' => auth('sanctum')->user()
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            'no_telp' => 'required',
+            'password' => 'required|min:8|max:64'
+        ]);
+
+        if (Auth::attempt($request->all())) {
+            $user = Auth::user();
+            $token = $request->user()->createToken('api-android')->plainTextToken;
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'User berhasil login',
+                'data' => $user,
+                'token' => $token
+            ]);
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'nama' => 'required|min:3|max:72',
+            'no_telp' => 'required|unique:users',
+            'tanggal_lahir' => 'required|date|before:today',
+            'password' => 'required|min:8|max:64|confirmed'
+        ]);
+
+        $user = User::create([
+            'nama' => $request->nama,
+            'no_telp' => $request->no_telp,
+            'tanggal_lahir' => $request->tanggal_lahir,
+            'password' => Hash::make($request->password),
+            'role_id' => Role::where('nama', 'User')->first()->id
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User berhasil didaftarkan',
+            'data' => $user
+        ]);
+    }
+
+    public function logout()
+    {
+        auth('sanctum')->user()->tokens()->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'User berhasil logout'
+        ]);
+    }
+
+    public function changePassword(Request $request)
+    {
+        $request->validate([
+            'old_password' => 'required|min:8|max:64|',
+            'new_password' => 'required|min:8|max:64|confirmed'
+        ]);
+
+        $user = auth('sanctum')->user();
+
+        if (Hash::check($request->old_password, $user->password)) {
+            if ($request->old_password == $request->new_password) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Password baru tidak boleh sama dengan password lama'
+                ]);
+            }
+
+            $user->password = Hash::make($request->new_password);
+            $user->save();
+
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Password berhasil diubah'
+            ]);
+        }
+
+        return response()->json([
+            'status' => 'error',
+            'message' => 'Password lama tidak sesuai'
+        ]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $request->validate([
+            'nama' => 'min:3|max:72',
+            'no_telp' => 'unique:users,no_telp',
+            'tanggal_lahir' => 'date|before:today'
+        ]);
+
+        $user = auth('sanctum')->user();
+        $user->update($request->all());
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Profil berhasil diubah',
+            'data' => $user
+        ]);
+    }
+}
---
+++ database/migrations/2024_05_13_065617_create_payments_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->string('payment_method')->nullable();
+            $table->string('payment_method_code')->nullable();
+            $table->enum('status', ['FAILED', 'PAID', 'EXPIRED', 'UNPAID'])->default('UNPAID');
+            $table->integer('total');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('payments');
+    }
+};
