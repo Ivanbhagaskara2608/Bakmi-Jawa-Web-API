4cfc12dcbdbdfb57d793ee2edf7bc6445b09e30c
Ivanbhagaskara2608 <ivanbhagas2003@gmail.com>
2024-05-16 08:25:12 +0700

    add logo

--- resources/views/Dashboard/app.blade.php
+++ resources/views/Dashboard/app.blade.php
@@ -31,7 +31,7 @@
 
   <!-- Preloader -->
   <div class="preloader flex-column justify-content-center align-items-center">
-    <img class="animation__shake" src="{{ asset('dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
+    <img class="animation__shake" src="{{ asset('dist/img/Logo_Bakmi.png') }}" alt="AdminLTELogo" height="200" width="200">
   </div>
 
   @include('Dashboard.partials.navbar')
